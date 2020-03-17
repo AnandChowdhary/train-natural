71618125bae56ee9a74c3a0a1a580b8f4c81e6b9
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-17 14:37:56 +0530

    :sparkles: Add basic tests, natural as dev dependency

--- index.spec.ts
+++ index.spec.ts
@@ -1,1 +1,20 @@
-it("tests", () => expect(1).toBeTruthy());
+import { train } from "./index";
+import { BayesClassifier } from "natural";
+import { join } from "path";
+import { writeFile, mkdirp } from "fs-extra";
+
+it("trains classifier", async () => {
+  const classifier = new BayesClassifier();
+  const result = await train(classifier, join(".cache"));
+  expect(result).toBeTruthy();
+});
+
+beforeAll(async () => {
+  await mkdirp(join(".cache", "india"));
+  await mkdirp(join(".cache", "the-netherlands"));
+
+  await writeFile(
+    join(".cache", "india", "1.txt"),
+    "India is a country in south Asia"
+  );
+});
